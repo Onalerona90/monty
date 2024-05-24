85e005348c876b5308f64ce5128c1dd62d48ca3a
Onalerona Pako <onapako90@gmail.com>
2024-05-24 13:16:05 +0200

    stack, queue

--- queue_op.c
+++ queue_op.c
@@ -8,35 +8,7 @@
 
 void queue_op(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp;
-	stack_t *last;
-
 	(void)stack;
 	(void)line_number;
-
-	if (data_structure_mode == STACK)
-	{
-		temp = *stack;
-		last = NULL;
-
-		if (*stack == NULL)
-			return;
-
-		while (temp->next != NULL)
-			temp = temp->next;
-
-		last = temp;
-		while (last->prev != NULL)
-		{
-			last->prev->next = last->next;
-			if (last->next != NULL)
-				last->next->prev = last->prev;
-			last->next = last->prev;
-			last->prev = NULL;
-			last->next->prev = last;
-			*stack = last;
-			last = last->next;
-		}
-		data_structure_mode = QUEUE;
-	}
+	data_structure_mode = QUEUE;
 }
