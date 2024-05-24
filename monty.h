1d9afd430f034258f36cdff483e9b0d387f6099e
Tebogo Ndaba <dennistebogo@gmail.com>
2024-05-24 09:49:39 +0200

    push, pall

--- monty.h
+++ monty.h
@@ -38,8 +38,6 @@ typedef struct instruction_s
 
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
-void free_stack(stack_t *stack);
-void execute(FILE *file);
 
 extern instruction_t instructions[];
 
