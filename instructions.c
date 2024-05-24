b78a755d2e5dd0d18ca1fa86760893fefe56bfa7
Onalerona Pako <onapako90@gmail.com>
2024-05-24 11:22:50 +0200

    The nop opcode

--- instructions.c
+++ instructions.c
@@ -7,5 +7,6 @@ instruction_t instructions[] = {
 	{"pop", pop},
 	{"swap", swap},
 	{"add", add},
+	{"nop", nop},
 	{NULL, NULL}
 };
--- monty.h
+++ monty.h
@@ -44,6 +44,7 @@ void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
 int is_integer(const char *str);
 void execute(char *line, unsigned int line_number, stack_t **stack);
 
